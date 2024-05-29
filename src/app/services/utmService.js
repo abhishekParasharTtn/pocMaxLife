ea1cfc63639896698f5d07645e2a20df629a699f
abhishekParasharTtn <abhishek.parashar@tothenew.com>
2024-05-29 16:25:28 +0530

    code update

--- src/app/services/utmService.js
+++ src/app/services/utmService.js
@@ -6,6 +6,34 @@ export const utmService = {
   transformUtmDetails: function (utmDetails) {
     return utmDetails?.[0];
   },
+  transformPageData: function (data) {
+    const transformData = data?.map((page) => {
+      console.log(page, "::page");
+      const { pages: _page = {} } = page;
+      const { sections = {}, layout } = _page;
+      const { form = {} } = sections;
+      const {
+        layout: { name },
+        ...layoutData
+      } = layout;
+      console.log(_page, "::page");
+      return {
+        name: _page?.name,
+        id: sections?.id,
+        route: {
+          current: _page?.slug,
+          previous: _page?.next?.previous?.url,
+          next: _page?.previous?.next?.url,
+        },
+        layout: {
+          name,
+          ...layoutData,
+        },
+        form,
+      };
+    });
+    return transformData;
+  },
   getUtmDetails: async function (slug) {
     const utmCode = slug[0];
     const utmDetails = await api.get(
@@ -18,6 +46,8 @@ export const utmService = {
   getThemeConfig: async function (utmDetails) {
     const query = queries.getThemeConfig(utmDetails?.themeConfig?.name);
     const themeConfig = await api.get(null, query);
+    console.log(themeConfig, "::themeConfig");
+    console.log(transformer.removeDataAttributes(themeConfig));
     return transformer.removeDataAttributes(themeConfig);
   },
   getPages: async function (utmDetails) {
@@ -33,14 +63,21 @@ export const utmService = {
         return data;
       })
     );
-    console.log(pageData, "::pageData");
-    fs.writeFile("output.json", JSON.stringify(pageData, null, 2), (err) => {
-      if (err) {
-        console.error("Error writing file", err);
-      } else {
-        console.log("Successfully wrote file");
+    const filterPageData = transformer.removeDatakeys(pageData);
+    console.log(this.transformPageData(filterPageData));
+    console.log(transformer.removeDatakeys(pageData), "::pageData");
+
+    fs.writeFile(
+      "output.json",
+      JSON.stringify(this.transformPageData(filterPageData), null, 2),
+      (err) => {
+        if (err) {
+          console.error("Error writing file", err);
+        } else {
+          console.log("Successfully wrote file");
+        }
       }
-    });
+    );
     return pageData;
     //return transformer.removeDataAttributes(pageData);
   },
--- src/utils/transformer.js
+++ src/utils/transformer.js
@@ -1,4 +1,26 @@
 export const transformer = {
+  removeDatakeys: function (obj) {
+    if (typeof obj !== "object" || obj === null) {
+      return obj;
+    }
+
+    if (Array.isArray(obj)) {
+      if (obj.length === 1) {
+        return transformer.removeDatakeys(obj[0]);
+      }
+      return obj.map(this.removeDatakeys);
+    }
+
+    return Object.entries(obj).reduce((acc, [key, value]) => {
+      if (key === "data" || key === "attributes" || key === "0") {
+        // Merge properties of the nested object into the accumulator
+        return { ...acc, ...transformer.removeDatakeys(value) };
+      }
+      // Recursively process other properties
+      return { ...acc, [key]: transformer.removeDatakeys(value) };
+    }, {});
+  },
+
   removeDataAttributes: function (obj) {
     if (typeof obj !== "object" || obj === null) {
       return obj;
