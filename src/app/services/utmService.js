a80a5f0a226a14125da6f3dcd05a20f05ec1ff32
abhishekParasharTtn <abhishek.parashar@tothenew.com>
2024-05-31 11:30:14 +0530

    code update

--- src/app/services/utmService.js
+++ src/app/services/utmService.js
@@ -5,10 +5,21 @@ export const utmService = {
   transformUtmDetails: function (utmDetails) {
     return utmDetails?.[0];
   },
+  dataFilter: function (array, criteria) {
+    if (criteria.length === 0) {
+      return array;
+    } else {
+      return array.filter((item) =>
+        criteria.some((criterion) => item[criterion.key] === criterion.value)
+      );
+    }
+  },
   transformPageData: async function (data, utmCode) {
     const gendersData = await utmService.getGendersData(utmCode);
     console.log(transformer.removeDatakeys(gendersData), "::test");
     const transformData = [];
+    const dataStore = [];
+    console.log(dataStore, "::test");
     data?.map((pages) => {
       return pages.pages.map((_page) => {
         const { sections = {}, layout = {} } = _page;
@@ -31,12 +42,73 @@ export const utmService = {
               return component;
             })
             .map((component) => {
-              if (component.title === "Gender") {
-                return {
-                  ...component,
-                  data: gendersData?.utmConfigs[0]?.dataConfig?.dataGenders,
-                };
+              //----------------------------------------------------------------
+              if (component?.dataSourceName?.name) {
+                const modifieUrl = component?.dataSourceName?.name
+                  ?.replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`)
+                  .toLowerCase();
+                dataStore.push({
+                  url: modifieUrl,
+                  dataSourceName: component?.dataSourceName?.name,
+                });
+                const optionData = fetch(
+                  `http://localhost:1338/api/${modifieUrl}?populate=*`
+                )
+                  .then((res) => res.json())
+                  .then((data) => {
+                    //cb = data;
+                    if (data?.data?.length > 0) {
+                      // console.log();
+                      // console.log(
+                      //   utmService.dataFilter(
+                      //     transformer.removeDatakeys(optionData?.data),
+                      //     component?.dataFilter
+                      //   ),
+                      //   "::opetaionData"
+                      // );
+                      return {
+                        ...component,
+                        data: utmService.dataFilter(
+                          transformer.removeDatakeys(data?.data),
+                          component?.dataFilter
+                        ),
+                      };
+                    }
+                    console.log(data);
+                    //return data;
+                  });
+                //const cd = optionData.then((data) => data);
+                //console.log(optionData.then((data) => {}));
+
+                // api.get(
+                //   `/api/${modifieUrl}?populate=*`
+                // ).then;
+                //dataStore.push()
+                // if (optionData?.data?.length > 0) {
+                //   console.log();
+                //   console.log(
+                //     utmService.dataFilter(
+                //       transformer.removeDatakeys(optionData?.data),
+                //       component?.dataFilter
+                //     ),
+                //     "::opetaionData"
+                //   );
+                //   return {
+                //     ...component,
+                //     data: utmService.dataFilter(
+                //       transformer.removeDatakeys(optionData?.data),
+                //       component?.dataFilter
+                //     ),
+                //   };
+                // }
               }
+
+              // if (component.title === "Gender") {
+              //   return {
+              //     ...component,
+              //     data: gendersData?.utmConfigs[0]?.dataConfig?.dataGenders,
+              //   };
+              // }
               return component;
             });
           return {
@@ -63,6 +135,16 @@ export const utmService = {
           sections: filteredSections,
         });
       });
+    });
+    console.log(dataStore, "::test");
+    const dataConfig = [api.get(`/api/${utmCode}?populate=*`)];
+    Promise.allSettled(
+      dataStore?.map((item) => {
+        let data = api.get(`/api/${item.url}?populate=*`);
+        return { data, dataSourceName: item?.dataSourceName };
+      })
+    ).then((data) => {
+      console.log(data, "::datayyy");
     });
     return transformData;
   },
