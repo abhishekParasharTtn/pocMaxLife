9d5df95078c1c4646ec627c34f26596c2132115c
rajeev.sharma <rajeev.sharma@tothenew.com>
2024-06-06 18:14:15 +0530

    product-detail-form: changes done for product selector

--- src/app/feature/component/Component.js
+++ src/app/feature/component/Component.js
@@ -1,15 +1,40 @@
 import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import FieldComponent from "@/app/feature/fieldComponents/fieldComponent";
+import { productService } from "@/app/services/productService/productService";
 
 const Component = ({
     themeConfig,
     utmConfig,
     form = {},
     formName
-    
+
 }) => {
     const { form: { components } = {} } = form;
-  
+    // const formData = useSelector()
+
+
+    // const formData = {
+    //       InsurerAge: 30,
+    //       premiumType: 'Limited Pay',
+    //       isPosSeller: 'Yes',
+    //       premiumPaymentTerm: '13',
+    //     }
+
+    if (form?.name === "productDetails") {
+        const formData = useSelector((state) => state.forms.personalDetails);
+        const productComponent = productService.getProductComponent('SSP', formData)
+        components?.forEach(field => {
+            if (productComponent[field?.name]) {
+                field.data = [];
+                field.data = productComponent[field?.name]?.values;
+            }
+        });
+
+    }
+    // console.log('productComponent', productComponent)
+
+    // console.log("form================>", form);
     return (
         <div className="grid grid-cols-2 gap-10">
             {
