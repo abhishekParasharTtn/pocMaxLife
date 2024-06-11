288a2bf2f1f21f6c1f13a9bbe621d63922226485
abhishekParasharTtn <abhishek.parashar@tothenew.com>
2024-06-11 12:57:11 +0530

    height issue fix

--- src/app/feature/Layout/PersonalDetailLayout.js
+++ src/app/feature/Layout/PersonalDetailLayout.js
@@ -1,38 +1,39 @@
-import React from 'react'
-import Section from '../Section/Section';
-import { extractDetailsSection, extractRoute } from '@/app/services/util';
+import React from "react";
+import Section from "../Section/Section";
+import { extractDetailsSection, extractRoute } from "@/app/services/util";
 
-const PersonalDetailLayout = ({ themeConfig, utmConfig, page,formName }) => {
-
-    const pageRoute = {
-        prev: page?.previous,
-        next: page?.next
-      }
+const PersonalDetailLayout = ({ themeConfig, utmConfig, page, formName }) => {
+  const pageRoute = {
+    prev: page?.previous,
+    next: page?.next,
+  };
 
   const sectionHandler = () => {
-    return <>
-      {page?.sections?.length > 0
-        && page.sections.map((section, index) => {
-          return <Section
-            key={index}
-            themeConfig={themeConfig}
-            utmConfig={utmConfig}
-            section={section}
-            formName={formName}
-            pageRoute={pageRoute}
-          // page={page}
-          />
-        })
-      }
-    </>
-  }
+    return (
+      <>
+        {page?.sections?.length > 0 &&
+          page.sections.map((section, index) => {
+            return (
+              <Section
+                key={index}
+                themeConfig={themeConfig}
+                utmConfig={utmConfig}
+                section={section}
+                formName={formName}
+                pageRoute={pageRoute}
+                // page={page}
+              />
+            );
+          })}
+      </>
+    );
+  };
 
   return (
-    <div className='bg-default px-8 pt-6 w-2/4'>
-      <div className={''}> {sectionHandler()}</div>
+    <div className="bg-default px-8 pt-6 w-2/4 h-fit">
+      <div className={""}> {sectionHandler()}</div>
     </div>
+  );
+};
 
-  )
-}
-
-export default PersonalDetailLayout
+export default PersonalDetailLayout;
--- src/app/feature/formLayout/Form.js
+++ src/app/feature/formLayout/Form.js
@@ -2,23 +2,23 @@ import React from "react";
 import dynamic from "next/dynamic";
 import ButtonGroup from "../component/ButtonGroup";
 
-
-const Form = ({ themeConfig, utmConfig, section,formName,pageRoute }) => {
-
-
-  const personalFormComponents = dynamic(() => import('../formComponent/personalFormComponents'));
-  const customerFormComponents = dynamic(() => import('../formComponent/customerFormComponents'));
+const Form = ({ themeConfig, utmConfig, section, formName, pageRoute }) => {
+  const personalFormComponents = dynamic(() =>
+    import("../formComponent/personalFormComponents")
+  );
+  const customerFormComponents = dynamic(() =>
+    import("../formComponent/customerFormComponents")
+  );
 
   const FormMappingLayout = {
     PersonalDetailsFormLayout: personalFormComponents,
-    CustomerDetailsFormLayout: customerFormComponents
-  }
+    CustomerDetailsFormLayout: customerFormComponents,
+  };
   const FormMapping = FormMappingLayout[section?.layout?.layout?.name] || null;
   const handleFormSubmit = (e) => {
     e.preventDefault();
     console.log("Form submitted!");
-    
-};
+  };
 
   return FormMapping ? (
     <form onSubmit={handleFormSubmit}>
@@ -29,27 +29,24 @@ const Form = ({ themeConfig, utmConfig, section,formName,pageRoute }) => {
         formName={formName}
         pageRoute={pageRoute}
       />
-      {
-        section?.button?.length > 0 ?
-          <div className='Section-footer flex flex-row-reverse'>
-            {
-              section.button.map((button, index) => {
-                return <ButtonGroup
-                  key={index}
-                  button={button}
-                  pageRoute={pageRoute}
-                  formName={formName}
-                />
-              })
-            }
-          </div> :
-          null
-      }
+      {section?.button?.length > 0 ? (
+        <div className="Section-footer flex flex-row-reverse">
+          {section.button.map((button, index) => {
+            return (
+              <ButtonGroup
+                key={index}
+                button={button}
+                pageRoute={pageRoute}
+                formName={formName}
+              />
+            );
+          })}
+        </div>
+      ) : null}
     </form>
   ) : (
     <>not found</>
-  )
+  );
+};
 
-}
-
-export default Form
+export default Form;
