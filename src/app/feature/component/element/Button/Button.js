34c91988242311cf37ed46086de790fb8b22663e
abhishekParasharTtn <abhishek.parashar@tothenew.com>
2024-06-11 12:38:35 +0530

    ui update

--- src/app/feature/component/element/Button/Button.js
+++ src/app/feature/component/element/Button/Button.js
@@ -1,39 +1,36 @@
-'use client'
-import { useRouter } from 'next/navigation'
-import { useSelector, useDispatch } from 'react-redux';
-import Link from 'next/link';
+"use client";
+import { useRouter } from "next/navigation";
+import { useSelector, useDispatch } from "react-redux";
+import Link from "next/link";
 
-const Button = ({button, component,pageRoute,formName}) => {
-    const {type, label, name } = button || component;
+const Button = ({ button, component, pageRoute, formName }) => {
+  const { type, label, name } = button || component;
 
-    const router = useRouter();
-    const formData = useSelector((state) => state.forms.personalDetails);
-   
-    const clickHandler = (e) => {
-        
-        
-        if (name === 'save' ) {
+  const router = useRouter();
+  const formData = useSelector((state) => state.forms.personalDetails);
 
-        }
-
-        else {
-            const route =  pageRoute?.next
-            if (route) router.push((route).replace('/', ''));  
-        }
-        
-        
+  const clickHandler = (e) => {
+    if (name === "save") {
+    } else {
+      const route = pageRoute?.next;
+      if (route) router.push(route.replace("/", ""));
     }
-   
-    return (
-            <div className="inline-block m-6 ">
-                <button
-                onClick={clickHandler}
-                    className={`${name === 'save' ? 'shadow-type2 w-48 bg-light h-12 inline-block ml-36' : 'text-white w-48 bg-light h-12  bg-primary inline ml-35'}`}>
-                    {label}
-                </button>
-            </div>
+  };
 
-    );
+  return (
+    <div className="inline-block my-5 ">
+      <button
+        onClick={clickHandler}
+        className={`${
+          name === "save"
+            ? "shadow-type2 w-48 bg-light h-12 inline-block ml-36"
+            : "text-white w-48 bg-light h-12  bg-primary inline ml-35"
+        }`}
+      >
+        {label}
+      </button>
+    </div>
+  );
 };
 
 export default Button;
