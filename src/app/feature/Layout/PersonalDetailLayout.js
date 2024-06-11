4ba365a695ea0b6b0f5b5e2df9aeca1776f623b0
Parikshit Malik <parikshitmalik@monocept.com>
2024-06-11 13:50:39 +0530

    conflict resolve

--- src/app/feature/Layout/PersonalDetailLayout.js
+++ src/app/feature/Layout/PersonalDetailLayout.js
@@ -30,13 +30,8 @@ const PersonalDetailLayout = ({ themeConfig, utmConfig, page, formName }) => {
   };
 
   return (
-<<<<<<< HEAD
     <div className='bg-default px-8 pt-6 w-7/12'>
       <div className={''}> {sectionHandler()}</div>
-=======
-    <div className="bg-default px-8 pt-6 w-2/4 h-fit">
-      <div className={""}> {sectionHandler()}</div>
->>>>>>> 5ee07588126028e49b2f5298a3d7599d8ac54c69
     </div>
   );
 };
